a87a3b356b9df485a9bb184b1bf61ce53ca453a3
convexstyle <convexstyle@gmail.com>
2013-09-30 09:42:59 +1000

    Initial Commit

---
+++ NewsReader/NRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRAppDelegate.h
+//  NewsReader
+//
+//  Created by convexstyle on 30/09/13.
+//  Copyright (c) 2013 convexstyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewsReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NewsReader
+//
+//  Created by convexstyle on 30/09/13.
+//  Copyright (c) 2013 convexstyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRAppDelegate class]));
+    }
+}
